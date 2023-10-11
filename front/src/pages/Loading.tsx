dfef5767dedfa5e366c467ed242d14bcfb6ae4bd
euno <chonist@gmail.com>
2023-10-11 20:21:09 +0900

    loading page update

--- front/src/pages/Loading.tsx
+++ front/src/pages/Loading.tsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const Loading = () => {
   return (
     <LoadingWrapper>
+      <img src={`${process.env.PUBLIC_URL}/img/loading.png`}></img>
       <img src={`${process.env.PUBLIC_URL}/img/loading.gif`}></img>
-      <span>NARANG</span>
     </LoadingWrapper>
   );
 };
@@ -15,15 +15,19 @@ export default Loading;
 const LoadingWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: white;
+  background-color: whitesmoke;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  span {
-    /* margin-top: 30px; */
-    color: #d7dbef;
-    font-size: 3em;
-    /* font-weight: 800; */
+  img:first-child {
+    width: 150px;
+    height: 150px;
+    object-fit: contain;
+  }
+  img:nth-child(2) {
+    width: 100px;
+    height: 100px;
+    object-fit: contain;
   }
 `;
