add50aeb9986d81fd8d3fac89410ab5bc7f725a0
euno <chonist@gmail.com>
2023-12-23 16:16:32 +0900

    Img coomponent error fix

--- front/src/components/common/Img.tsx
+++ front/src/components/common/Img.tsx
@@ -9,6 +9,8 @@ const Img = ({
   altImg,
   crop,
   loading,
+  width,
+  height,
   onClick
 }: {
   className?: string;
@@ -17,26 +19,17 @@ const Img = ({
   alt: string;
   altImg?: string;
   crop?: boolean;
+  width?: string;
+  height?: string;
   loading?: "lazy" | "eager" | undefined;
 
   onClick?: () => void;
 }) => {
   const [errorCount, setErrorCount] = useState<number>(0);
-  const [altImage, setAltImage] = useState<string>(altImg ? altImg : "");
   const failImg = "/img/loadingFailed.png";
 
   //에러 발생 시 altImg로 한번 시도하고 또 에러가 발생하면 기본 이미지가 나오도록한다. 무한 루프가 방지된다.
 
-  useEffect(() => {
-    if (errorCount != 0) {
-      // console.log(src);
-      // console.log(errorCount);
-      //5가 되기 전까지 alt src 시도, 5일때 실패 이미지로 변경
-      if (errorCount >= 10) {
-        setAltImage(failImg);
-      }
-    }
-  }, [errorCount]);
   return (
     <ImgSC
       onClick={onClick}
@@ -46,11 +39,16 @@ const Img = ({
       id={id}
       src={src}
       alt={alt}
+      width={width}
+      height={height}
       onError={(e) => {
         console.log("error occur in image loading");
-        e.currentTarget.src = altImage;
+        e.currentTarget.src = altImg ? altImg : "";
         if (errorCount < 10) {
           setErrorCount((c) => (c += 1));
+        }
+        if (errorCount >= 10) {
+          e.currentTarget.src = failImg;
         }
       }}
     />
