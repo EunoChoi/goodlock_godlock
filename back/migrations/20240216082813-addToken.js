ea57241b08ef1a12098032207c020bb2464ad6d2
root <root@ip-172-31-34-8.ap-northeast-2.compute.internal>
2024-04-16 18:49:30 +0900

    db migration code fix

--- back/migrations/20240216082813-addToken.js
+++ back/migrations/20240216082813-addToken.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'refreshToken', {
+    await queryInterface.addColumn('Users', 'refreshToken', {
       type: Sequelize.STRING(300)
     })
     /**
